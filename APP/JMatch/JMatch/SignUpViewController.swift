6bff31569c239c43a11c2d032567cfa7f76c3d1b
柯元豪 <houhou1106@gmail.com>
2020-05-26 18:02:13 +0800

    connect firebase

--- APP/JMatch/JMatch/SignUpViewController.swift
+++ APP/JMatch/JMatch/SignUpViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
 class SignUpViewController: UIViewController, UITextFieldDelegate {
     
@@ -171,5 +174,42 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
         // Pass the selected object to the new view controller.
     }
     */
+    // MARK: - TO Firebase
+    
+    var uid = ""
+    
+    @IBAction func CreateAccount(_ sender: UIButton) {
+        if txtEmail.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
 
+        }
+        else {
+            Auth.auth().createUser(withEmail: txtEmail.text!, password: txtPassword.text!) { (user, error) in
+                
+                if error == nil {
+                    if let user = Auth.auth().currentUser{
+                        self.uid = user.uid
+                    }
+                    Database.database().reference(withPath: "ID/\(self.uid)/Profile/Safety-Check").setValue("ON")
+                    print("You have successfully signed up")
+                    //Goes to the Setup page which lets the user take a photo for their profile picture and also chose a username
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "LoginVC")
+                    self.present(vc!, animated: true, completion: nil)
+                    
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
 }
